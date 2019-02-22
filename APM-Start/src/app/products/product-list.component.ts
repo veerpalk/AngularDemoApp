adedc83f65b91fce6f277ad7c983dcf2fcf7ff1e
Veerpal kaur <veerpal.kaur@nagarro.com>
2019-02-22 10:38:41 +0530

    initial commit

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,68 @@
+import{Component,OnInit} from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { error } from '@angular/compiler/src/util';
+
+
+@Component({
+    selector:'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls:['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit{
+    pageTitle: string ='Product List';
+    imageWidth:number =50;
+    imageMargin :number =2;
+    showImage:boolean =false;
+    _listFilter:string;
+    errorMessage:string;
+    
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter=value;
+        this.filteredProducts=this.listFilter?this.performFilter(this.listFilter):this.products;
+
+
+    }
+    filteredProducts:IProduct[];
+    products: IProduct[]=[];
+
+      constructor(private productService:ProductService){      }
+
+
+      onRatingClicked(message:string):void {
+          this.pageTitle='Product List '+message;
+      }
+
+
+      performFilter(filterBy:string):IProduct[]
+      {
+          filterBy=filterBy.toLocaleLowerCase();
+      //    return this.products.filter((product:IProduct)=>
+        //      product.productName.toLocaleLowerCase().indexOf(filterBy)! == -1);
+              return this.products.filter((product:IProduct)=>
+              product.productName.toLocaleLowerCase().includes(filterBy));
+   
+            }
+      
+
+      toggleImage():void{
+          this.showImage=!this.showImage;
+      }
+      ngOnInit():void{
+       
+      this.productService.getProducts().subscribe(
+           products=> {this.products=products,
+           this.filteredProducts=this.products;},
+           error=>this.errorMessage=<any> error
+       );
+          
+      
+       
+      }
+  
+
+}
